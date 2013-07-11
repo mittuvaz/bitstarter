4645ad648ad4140b3a2037bfd4e9e90b8af060b6
mittuvaz <mittu_vaz@yahoo.com>
2013-07-11 06:53:39 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var file = fs.readFileSync('index.html');
+var fs = require ('fs');
 var app = express.createServer(express.logger());
-
+//fs.readFileSync("index.html"),"utf-8");
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(file.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
